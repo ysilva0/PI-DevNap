74ead54b7cbd8188b36466fc7ce960391ad94ef9
Fernando Ribeiro de Amorim <nandoamori@yahoo.com.br>
2022-05-05 14:51:22 -0300

    Confirmar

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
+app.use(express.json())
 
 
 app.listen(3000, () => console.log('Aplicação rodando em http://localhost:3000'))
