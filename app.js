4f436ec5147720b3916bdd5e01accf37aa66cadc
tryrox <raffa.davm.familia@gmail.com>
2022-05-10 10:44:14 -0300

    adicionado rotas

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mainRouter= require('../PI-DevNap/src/routes/mainRouter')
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
 
+
+app.use('/', mainRouter)
 
 app.listen(3000, () => console.log('Aplicação rodando em http://localhost:3000'))
---
+++ src/routes/mainRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mainController = require('../controllers/mainController')
+const mainRouter = express.Router();
+
+mainRouter.get('/', mainController.index)
+
+module.exports = mainRouter; 
