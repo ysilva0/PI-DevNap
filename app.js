36c2017b855f6c2938949250ab98026b38643320
tryrox <raffa.davm.familia@gmail.com>
2022-05-10 10:13:31 -0300

    adição do ejs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require('path')
 
 app.use(express.json())
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 
 app.listen(3000, () => console.log('Aplicação rodando em http://localhost:3000'))
