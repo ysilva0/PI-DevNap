96227247b5b8891bb2ddee26776f0958297e04b1
Ygor Silva <ygor.s2017@gmail.com>
2022-06-12 01:55:13 -0300

    header da home

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'src', 'views'))
 
 
